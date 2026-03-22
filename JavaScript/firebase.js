e3bb35b738629fb2a953ca2dceaf67440b678beb
Shebek3000 <shebek3000@gmail.com>
2026-03-22 18:12:30 +0200

    Set up Firebase app and Firestore integration
    Initialize Firebase app and Firestore database.

--- JavaScript/firebase.js
+++ JavaScript/firebase.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } 
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD-Om2W5eygZTRvoRFEuyoot8OzUSP8-lw",
+  authDomain: "demeets.firebaseapp.com",
+  projectId: "demeets"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+window.firebaseStuff = { collection, addDoc, getDocs, deleteDoc, doc };
